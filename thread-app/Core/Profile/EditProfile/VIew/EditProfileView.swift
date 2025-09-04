3e2ecdf60f843c67163db81325aa7a758a9e8918
hashanranasinghe <hashan.ranasinghe98@gmail.com>
2025-09-04 21:22:21 +0530

    add uis

---
+++ thread-app/Core/Profile/EditProfile/VIew/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  thread-app
+//
+//  Created by Hashan Ranasinghe on 2025-09-04.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
